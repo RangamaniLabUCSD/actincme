f0972849ca4cbaa38f7ac239737b8cad219d06b3
Ritvik_Vasan <rvasan@eng.ucsd.edu>
2020-06-28 23:38:53 -0700

    removed mpl.use('TkAgg') from init.py

--- actincme/bin/__init__.py
+++ actincme/bin/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Bin scripts package for actincme."""
-import matplotlib as mpl
-mpl.use('TkAgg')
+# import matplotlib as mpl
+# mpl.use('TkAgg')
 
 from . import filament, membrane, rotate, symmetricize, my_example, mypyntcloud
